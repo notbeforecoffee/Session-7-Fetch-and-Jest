e709e3faf055365792ef4a8767b1873369b0e513
notbeforecoffee <theqcontinuum@msn.com>
2023-06-14 21:55:51 -0400

    updates ReadMe

--- 3-simple-fs-and-jest-example/index.js
+++ 3-simple-fs-and-jest-example/index.js
@@ -1,32 +1,32 @@
-
-import fs from "fs/promises"
+import fs from 'fs/promises';
 
 //First we are going to create an async function to get users
 
-export const getUsers = async(filePath) => {
+export const getUsers = async (filePath) => {
 
-try {
-  const data = await fs.readFile(filePath);
-  // console.log(data.toString());
-return JSON.parse(data)
-}catch (e){
-  console.log('e.message')
-}
+  try {
 
-}
+    const data = await fs.readFile(filePath);
+    console.log(data.toString());
+    return JSON.parse(data);
 
-
+  } catch (e) {
+    console.log('e.message');
+  }
+};
 
 // Let's create a function to render the data we retrieved onto our HTML, remember that your data is json! If you need help with recalling how to get values from an object, last week class material on arrays is helpful here!
 
 export const countUsers = async () => {
-  let users =  await getUsers('3-simple-fs-and-jest-example/data.json');
+  let users = await getUsers('3-simple-fs-and-jest-example/data.json');
 
-  // console.log('users: ', users)
+  const userCount = users.length;
 
-  const userCount = users.length
+  return userCount;
+};
 
-  return userCount
-
-}
 countUsers();
+
+
+
+
